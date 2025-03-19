ed9844684bfee0f408d8b45accc48aa03aa241eb
Mathias Mentzoni <mathimg@stud.ntnu.no>
2025-03-19 09:40:36 +0100

    Added some error constants and openmeteo URL

--- config/constants.go
+++ config/constants.go
@@ -1,6 +1,21 @@
 package config
 
 const START_URL = "/dashboard/" + VERSION
+
+// API URLs
+const (
+	RESTCOUNTRIES_ROOT = "http://129.241.150.113:8080/v3.1/"
+	CURRENCY_ROOT      = "http://129.241.150.113:9090/currency/"
+	OPENMETEO_ROOT     = "https://api.open-meteo.com/v1/forecast/"
+)
+
+// API version
 const VERSION = "v1"
-const RESTCOUNTRIES_ROOT = "http://129.241.150.113:8080/v3.1/"
-const CURRENCY_ROOT = "http://129.241.150.113:9090/currency/"
+
+// Error messages
+const (
+	ERR_NOT_FOUND             = "Not found"
+	ERR_INTERNAL_SERVER_ERROR = "Internal server error"
+	ERR_BAD_REQUEST           = "Bad request"
+	// Fill in with more as we go
+)
