a610e4f0b9af1b974a53387dd76320daf73ed6b3
Marius Aga <mnaga@stud.ntnu.no>
2025-03-24 14:07:48 +0100

    Added structs for registration and DB handler

--- utils/structs.go
+++ utils/structs.go
@@ -1,12 +1,31 @@
 package utils
 
+import "time"
+
 type Statusresponse struct {
-	CountriesAPI         int `firestore:"countriesAPI" json:"countriesAPI"`
-	CurrencyAPI          int `firestore:"currencyAPI" json:"currencyAPI"`
-	OpenmeteoAPI         int `firestore:"openmeteoAPI" json:"openmeteoAPI"`
-	Notificationresponse int `firestore:"notificationresponse" json:"notificationresponse"`
-	Dashboardresponse    int `firestore:"dashboardresponse" json:"dashboardresponse"`
-	Webhookssum          int `firestore:"webhookssum" json:"webhookssum"`
-	Version              int `firestore:"version" json:"version"`
-	Uptime               int `firestore:"uptime" json:"uptime"`
+	CountriesAPI         int    `firestore:"countriesAPI" json:"countriesAPI"`
+	CurrencyAPI          int    `firestore:"currencyAPI" json:"currencyAPI"`
+	OpenmeteoAPI         int    `firestore:"openmeteoAPI" json:"openmeteoAPI"`
+	Notificationresponse int    `firestore:"notificationresponse" json:"notificationresponse"`
+	Dashboardresponse    int    `firestore:"dashboardresponse" json:"dashboardresponse"`
+	Webhookssum          int    `firestore:"webhookssum" json:"webhookssum"`
+	Version              string `firestore:"version" json:"version"`
+	Uptime               string `firestore:"uptime" json:"uptime"`
+}
+
+type Dashboard struct {
+	Id         string    `firestore:"id" json:"id"`
+	Country    string    `firestore:"country" json:"country"`
+	IsoCode    string    `firestore:"isoCode" json:"isoCode"`
+	Features   Features  `firestore:"features" json:"features"`
+	LastChange time.Time `firestore:"lastChange" json:"lastChange"`
+}
+type Features struct {
+	Temperature      bool     `firestore:"temperature" json:"temperature"`
+	Precipitation    bool     `firestore:"precipitation" json:"precipitation"`
+	Capital          bool     `firestore:"capital" json:"capital"`
+	Coordinates      bool     `firestore:"coordinates" json:"coordinates"`
+	Population       bool     `firestore:"population" json:"population"`
+	Area             bool     `firestore:"area" json:"area"`
+	TargetCurrencies []string `firestore:"targetCurrencies" json:"targetCurrencies"`
 }
