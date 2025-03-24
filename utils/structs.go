ce90b0ee5ed595f9b29a01ae1542cd825dec1657
sebasama <sebasama@stud.ntnu.no>
2025-03-24 14:03:45 +0100

    added struct for status handler

--- utils/structs.go
+++ utils/structs.go
@@ -1,1 +1,12 @@
 package utils
+
+type Statusresponse struct {
+	countriesAPI         int `firestore:"countriesAPI" json:"countriesAPI"`
+	currencyAPI          int `firestore:"currencyAPI" json:"currencyAPI"`
+	openmeteoAPI         int `firestore:"openmeteoAPI" json:"openmeteoAPI"`
+	notificationresponse int `firestore:"notificationresponse" json:"notificationresponse"`
+	dashboardresponse    int `firestore:"dashboardresponse" json:"dashboardresponse"`
+	webhookssum          int `firestore:"webhookssum" json:"webhookssum"`
+	version              int `firestore:"version" json:"version"`
+	uptime               int `firestore:"uptime" json:"uptime"`
+}
