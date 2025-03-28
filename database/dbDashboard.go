29d41d47e41eccdad937bddeb41dcccf84146f43
sebasama <sebasama@stud.ntnu.no>
2025-03-29 00:01:56 +0100

    mostly done dashboardhandler and dbdashboard, still some problems with Getcurrenctrates and not tested if the struct response works as intended

--- database/dbDashboard.go
+++ database/dbDashboard.go
@@ -1,1 +1,144 @@
 package database
+
+import (
+	"assignment-2/config"
+	"assignment-2/utils"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetCountryData(name string, isoCode string) (*utils.CountryResponse, error) {
+	var url string
+
+	if isoCode != "" {
+		url = fmt.Sprintf("%s/alpha/%s", config.RESTCOUNTRIES_ROOT, isoCode)
+	} else if name != "" {
+		url = fmt.Sprintf("%s/name/%s", config.RESTCOUNTRIES_ROOT, name)
+	} else {
+		return nil, errors.New("no country name or isoCode provided")
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch country data: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Handle HTTP errors from external API
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("REST Countries API returned status %d", resp.StatusCode)
+	}
+
+	// Read API response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error: Failed to read API response. %w", err)
+
+	}
+
+	// Parse JSON response
+	var countryData []utils.CountryResponse
+	if err := json.Unmarshal(body, &countryData); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON response: %w", err)
+	}
+
+	// Ensure data is available
+	if len(countryData) == 0 {
+		return nil, fmt.Errorf("failed to parse JSON response: %w", err)
+	}
+
+	return &countryData[0], nil
+}
+
+func GetWeatherDate(latitude string, longitude string) (*utils.OpenMeteoresponse, error) {
+
+	url := fmt.Sprintf("%s?latitude=%s&longtitude=%s", config.RESTCOUNTRIES_ROOT, latitude, longitude)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch weather data: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Handle HTTP errors from external API
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("OpenMeteo API returned status %d", resp.StatusCode)
+	}
+
+	// Read API response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error: Failed to read API response. %w", err)
+
+	}
+
+	// Parse JSON response
+	var weatherData []utils.OpenMeteoresponse
+	if err := json.Unmarshal(body, &weatherData); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON response: %w", err)
+	}
+
+	// Ensure data is available
+	if len(weatherData) == 0 {
+		return nil, fmt.Errorf("failed to parse JSON response: %w", err)
+	}
+
+	return &weatherData[0], nil
+}
+
+func Average(numbers []float64) float64 {
+	sum := 0.0
+	for _, num := range numbers {
+		sum += num
+	}
+
+	var mean float64
+	if len(numbers) > 0 {
+		mean = sum / float64(len(numbers))
+	}
+
+	return mean
+}
+
+func GetCurrencyRates(curency []string, countrycode string) (*utils.CurrencyResponse, error) {
+	var fullcurrencydata []utils.CurrencyResponse
+
+	for _, cur := range curency {
+		url := config.RESTCOUNTRIES_ROOT + countrycode
+
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch weather data: %w", err)
+		}
+		defer resp.Body.Close()
+
+		// Handle HTTP errors from external API
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("OpenMeteo API returned status %d", resp.StatusCode)
+		}
+
+		// Read API response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("Error: Failed to read API response. %w", err)
+
+		}
+
+		// Parse JSON response
+		var CurrencyData []utils.CurrencyResponse
+		if err := json.Unmarshal(body, &CurrencyData); err != nil {
+			return nil, fmt.Errorf("failed to parse JSON response: %w", err)
+		}
+
+		// Ensure data is available
+		if len(CurrencyData) == 0 {
+			return nil, fmt.Errorf("failed to parse JSON response: %w", err)
+		}
+		fullcurrencydata[cur] = CurrencyData
+	}
+
+	return &fullcurrencydata[], nil
+}
--- handlers/dashboardHandler.go
+++ handlers/dashboardHandler.go
@@ -1,6 +1,16 @@
 package handlers
 
-import "net/http"
+import (
+	"assignment-2/config"
+	"assignment-2/database"
+	"assignment-2/utils"
+	"encoding/asn1"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -14,5 +24,106 @@ func DashboardHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleDashGetRequest(w http.ResponseWriter, r *http.Request) {
+	basePath := config.START_URL + "/dashboards/"
+	trimmedPath := strings.TrimPrefix(r.URL.Path, basePath)
+	parts := strings.Split(trimmedPath, "/")
 
+	// If an ID was provided, get one
+	if len(parts) >= 1 && parts[0] != "" {
+		http.Error("dasboard ID not provided", http.StatusBadRequest)
+		return
+	}
+	id := parts[0]
+
+	rawContent, err := database.GetOneRegistration(id)
+	if err != nil {
+		log.Println("Error retrieving dashboard with id " + id + ": " + err.Error())
+		http.Error(w, "There was an error getting the dashboard with id: "+id, http.StatusInternalServerError)
+		return
+	}
+
+	// Extract fields from rawConfig (stored in Firestore)
+	country := rawContent.Country
+	isoCode := rawContent.IsoCode
+	features := rawContent.Features
+	lastsignin := rawContent.LastChange
+
+	// Get country info
+	countryData, err := database.GetCountryData(country, isoCode)
+	if err != nil {
+		http.Error(w, "Failed to fetch country data", http.StatusBadGateway)
+		return
+	}
+
+	// Get weather info
+	weatherData, err := database.GetWeatherDate(countryData.Latling[0], countryData.Latling[1])
+	if err != nil {
+		http.Error(w, "Failed to fetch country data", http.StatusBadGateway)
+		return
+	}
+
+	precaverage := database.Average(weatherData.Precipitation)
+	tempaverage := database.Average(weatherData.Temperature)
+
+	// Prepare response
+	response := map[string]interface{}{
+		"country": country,
+		"isoCode": isoCode,
+		"features": utils.Featureseponse{},
+		"lastRetrival": lastsignin,
+
+	}
+
+	responseFeatures := response["features"].(map[string]interface{})
+
+	// Capital
+	if features.Capital {
+		responseFeatures["capital"] = countryData.Capital
+	}
+
+	// Coordinates
+	if features.Coordinates {
+		responseFeatures["coordinates"] = map[string]string{
+			"latitude":  countryData.Latling[0],
+			"longitude": countryData.Latling[1],
+		}
+	}
+
+	// Population
+	if features.Population {
+		responseFeatures["population"] = countryData.Population
+	}
+
+	// Area
+	if features.Area {
+		responseFeatures["area"] = countryData.Area
+	}
+
+	if features.Temperature {
+		responseFeatures["temperature"] = tempaverage
+	}
+
+	if features.Precipitation {
+		responseFeatures["precipitation"] = precaverage
+	}
+
+
+	// Currency
+	if tcs := features.TargetCurrencies; len(tcs) > 0 {
+
+		rates, err := database.GetCurrencyRates(tcs, countryData.Cca3)
+		if err != nil {
+			http.Error(w, "Currency API failed", http.StatusBadGateway)
+			return
+		}
+		responseFeatures["targetCurrencies"] = rates
+	}
+
+	// Trigger webhooks asynchronously
+	go services.TriggerWebhooks("INVOKE", isoCode)
+
+	// Send the final response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
 }
