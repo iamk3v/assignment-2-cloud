a3095d77be8a471ab8710a6ad05e410ea84bb82f
Marius Aga <mnaga@stud.ntnu.no>
2025-04-07 10:43:07 +0200

    Added another unit test case

--- handlers/registrationHandler_test.go
+++ handlers/registrationHandler_test.go
@@ -8,11 +8,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 )
 
 func TestGetOne(t *testing.T) {
-	id := "5NgZBNfIJA93lquV1n9R"
+	id := "FoVoLL4aeKvilKON3Wjl"
 	req := httptest.NewRequest(http.MethodGet, config.START_URL+"/registrations/"+id, nil)
 	w := httptest.NewRecorder()
 
@@ -24,10 +23,20 @@ func TestGetOne(t *testing.T) {
 		t.Fatalf("Error reading response body: %v", err)
 	}
 
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Unexpected status code: %d", resp.StatusCode)
+	}
+
+	var gotten utils.Dashboard
+	err = json.Unmarshal(body, &gotten)
+	if err != nil {
+		t.Fatalf("Error unmarshalling response body: %v", err)
+	}
+
 	expected := utils.Dashboard{
-		Id:      "5NgZBNfIJA93lquV1n9R",
-		Country: "Norway",
-		IsoCode: "NO",
+		Id:      "FoVoLL4aeKvilKON3Wjl",
+		Country: "Sweden",
+		IsoCode: "",
 		Features: utils.Features{
 			Temperature:      true,
 			Precipitation:    true,
@@ -37,16 +46,38 @@ func TestGetOne(t *testing.T) {
 			Area:             false,
 			TargetCurrencies: []string{"EUR", "USD", "SEK"},
 		},
-		LastChange: time.Now(),
 	}
 
-	// Marshal it to JSON to compare
-	expectedJSON, err := json.Marshal(expected)
+	if expected.Id != gotten.Id {
+		t.Errorf("Expected ID %s, got ID %s", expected.Id, gotten.Id)
+	}
+
+	if expected.Country != gotten.Country {
+		t.Errorf("Expected Country %s, got Country %s", expected.Country, gotten.Country)
+	}
+
+	if expected.Features.Temperature != gotten.Features.Temperature {
+		t.Errorf("Expected value %t, got value %t", expected.Features.Temperature, gotten.Features.Temperature)
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, config.START_URL+"/registrations/", nil)
+	w := httptest.NewRecorder()
+
+	RegistrationHandler(w, req) // Directly call the handler
+
+	resp := w.Result()
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		t.Fatalf("Error marshaling expected dashboard: %v", err)
+		t.Fatalf("Error reading response body: %v", err)
 	}
 
-	if string(body) != string(expectedJSON) {
-		t.Errorf("Expected %s, got %s", string(expectedJSON), string(body))
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Unexpected status code: %d", resp.StatusCode)
+	}
+
+	if len(body) == 0 || body == nil {
+		t.Fatalf("Registration response body is empty")
 	}
 }
