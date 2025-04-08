37a5a4e6cb5b53e2a58fecbc135373686af7be00
Marius Aga <mnaga@stud.ntnu.no>
2025-04-08 18:17:35 +0200

    Re-wrote error message

--- handlers/notFoundHandler.go
+++ handlers/notFoundHandler.go
@@ -9,8 +9,8 @@ import (
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	_, err := fmt.Fprintf(w, "404 - Not Found, only a large amount of dust found\n"+
-		strconv.Itoa(http.StatusNotFound))
+	_, err := fmt.Fprintf(w, strconv.Itoa(http.StatusNotFound)+" - "+http.StatusText(http.StatusNotFound)+
+		", only a large amount of dust found, double check your URL!")
 	if err != nil {
 		log.Print("Error occurred when trying to send response: " + err.Error())
 		http.Error(w, "An internal error occurred..", http.StatusInternalServerError)
