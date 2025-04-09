cebdaac7d592a529b47a04da9843f0e1d5d2349b
Marius Nicolai Holte Aga <mnaga@stud.ntnu.no>
2025-04-09 18:53:35 +0200

    Added html site for all 404 not found

--- handlers/notFoundHandler.go
+++ handlers/notFoundHandler.go
@@ -2,21 +2,40 @@ package handlers
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 /*
 NotFoundHandler Handles all requests to endpoints that does not yet exist.
 */
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	_, err := fmt.Fprintf(w, "%d - %s, only a large amount of dust found, double check your URL!",
-		http.StatusNotFound, http.StatusText(http.StatusNotFound))
-
+	file, err := os.Open("html/index.html")
 	if err != nil {
-		log.Print("Error occurred when trying to send response: " + err.Error())
-		http.Error(w, "An internal error occurred..", http.StatusInternalServerError)
-		return
+		log.Print("Error occurred when trying to open file: " + err.Error())
+		_, err := fmt.Fprintf(w, "%d - %s, only a large amount of dust found, double check your URL!",
+			http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		if err != nil {
+			log.Print("Error occurred when trying to send response: " + err.Error())
+			http.Error(w, "An internal error occurred..", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	defer file.Close()
+
+	w.WriteHeader(http.StatusNotFound)
+	_, err = io.Copy(w, file)
+	if err != nil {
+		log.Print("Error occurred when trying to copy file: " + err.Error())
+		_, err := fmt.Fprintf(w, "%d - %s, only a large amount of dust found, double check your URL!",
+			http.StatusNotFound, http.StatusText(http.StatusNotFound))
+		if err != nil {
+			log.Print("Error occurred when trying to send response: " + err.Error())
+			http.Error(w, "An internal error occurred..", http.StatusInternalServerError)
+			return
+		}
 	}
 }
